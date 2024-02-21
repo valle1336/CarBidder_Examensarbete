981939a30ac15c8dd20b65a5619151de33c2c86e
valle1336 <112933517+valle1336@users.noreply.github.com>
2024-02-21 14:33:38 +0100

    Börjat jobba med att kunna ladda upp bilar!

---
+++ src/main/java/com/alex/carbider/CarBider/entities/user/cars/CarEntityDetailsService.java
@@ -0,0 +1,8 @@
+package com.alex.carbider.CarBider.entities.user.cars;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class CarEntityDetailsService {
+
+}
---
+++ src/main/java/com/alex/carbider/CarBider/entities/user/cars/CarRepository.java
@@ -0,0 +1,11 @@
+package com.alex.carbider.CarBider.entities.user.cars;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CarRepository extends JpaRepository<CarEntity, Long> {
+
+    CarEntity findProductByTitle(String title);
+
+}
