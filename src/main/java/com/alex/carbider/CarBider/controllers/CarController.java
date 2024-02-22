868d55057d28b0f4af8c7c47ea2bf4ca7a4ee644
valle1336 <112933517+valle1336@users.noreply.github.com>
2024-02-22 15:10:03 +0100

    Skapat en edit page för "mina annonser" så att användare nu kan redigera sina egna annonser!

--- src/main/java/com/alex/carbider/CarBider/controllers/CarController.java
+++ src/main/java/com/alex/carbider/CarBider/controllers/CarController.java
@@ -15,6 +15,7 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -86,4 +87,53 @@ public class CarController {
 
         return "myCars";
     }
+
+    @GetMapping("/editMyCar")
+    public String showEditCarPage(
+            @RequestParam("carId")
+            Long carId,
+            Model model
+    ) {
+        CarEntity car = carRepository.findById(carId).orElse(null);
+
+        if (car == null) {
+            return "error.page";
+        }
+        model.addAttribute("carId", carId);
+        model.addAttribute("carTitle", car.getTitle());
+        model.addAttribute("carDescription", car.getDescription());
+        model.addAttribute("carStartingBid", car.getStartingBid());
+        model.addAttribute("carEmail", car.getEmail());
+        model.addAttribute("carBuyOutPrice", car.getBuyOutPrice());
+
+        return "editMyCar";
+    }
+
+    @PostMapping("/save-edit-car")
+    public String saveCar(
+            @RequestParam("carId") Long carId,
+            @RequestParam("title") String title,
+            @RequestParam("description") String description,
+            @RequestParam("email") String email,
+            @RequestParam("startingBid") int startingBid,
+            @RequestParam("buyOutPrice") int buyOutPrice)
+
+
+    {
+
+        CarEntity car = carRepository.findById(carId).orElse(null);
+        if (car == null) {
+            return "error-page";
+        }
+
+        car.setTitle(title);
+        car.setDescription(description);
+        car.setEmail(email);
+        car.setStartingBid(startingBid);
+        car.setBuyOutPrice(buyOutPrice);
+        carRepository.save(car);
+
+        return "redirect:/myCars";
+    }
+
 }
